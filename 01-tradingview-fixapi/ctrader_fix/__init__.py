e8346089d8dc40e9c300e091a34f3c6229ef7335
suyons <su02ga@outlook.com>
2024-05-24 20:56:48 +0900

    Send an order to cTrader FIX API
    Used package
    https://github.com/ejtraderLabs/ejtraderCT

--- 01-tradingview-fixapi/ctrader_fix/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Top-level package for cTraderFixPy."""
-from .client import *
-from .fixProtocol import *
-from .messages import *
-from .factory import *
-__author__ = """Spotware"""
-__email__ = 'connect@spotware.com'
--- 01-tradingview-fixapi/main.py
+++ 01-tradingview-fixapi/main.py
@@ -1,13 +1,68 @@
+import json, requests
+from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
-import requests
-import json
+from ctrader_fix.ctrader import Ctrader
+
 
 app = FastAPI()
 
+
 with open("config.json") as config_file:
     config = json.load(config_file)
+    HOST_NAME = config["Host"]
+    SENDER_COMPID = config["SenderCompID"]
+    PASSWORD = config["Password"]
     TELEGRAM_BOT_TOKEN = config["TelegramBotToken"]
     TELEGRAM_CHAT_ID = config["TelegramChatID"]
+global api
+api = Ctrader(HOST_NAME, SENDER_COMPID, PASSWORD)
+
+qty_mult: int = 2
+
+
+class LoginModel(BaseModel):
+    server: str
+    account: str
+    password: str
+
+
+class SymbolModel(BaseModel):
+    symbols: list[str]
+
+
+class OrderModel(BaseModel):
+    symbol: str
+    volume: float
+    stoploss: float = None
+    takeprofit: float = None
+    price: float = None
+
+
+class ModifyModel(BaseModel):
+    id: str
+    stoploss: float
+    takeprofit: float
+    price: float = None
+
+
+class IdModel(BaseModel):
+    id: str
+
+
+async def check():
+    return api.isconnected()
+
+
+async def telegram_request(tg_body: dict):
+    response = requests.post(
+        f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage",
+        json=tg_body,
+    )
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=500, detail="Telegram connection error.")
 
 
 @app.get("/")
@@ -15,29 +70,113 @@ async def hello_ctrader():
     return {"message": "Hello cTrader!"}
 
 
-@app.post("/")
-async def trade_signal(tv_body: dict):
-    """TradingView Webhook body example:
-     {
-        "symbol": "EURUSD",
-        "side": "Buy",
-        "price": "1.23456"
-    }
-    """
-    tg_body = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": "Trade signal received!\n\n"
-        + f"Symbol: {tv_body['symbol']}\n"
-        + f"Side: {tv_body['side']}\n"
-        + f"Price: {tv_body['price']}",
-    }
-    response = requests.post(
-        f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage", json=tg_body
-    )
+@app.post("/login")
+async def login():
+    api = Ctrader(HOST_NAME, SENDER_COMPID, PASSWORD)
+    return {"connected": api.isconnected()}
 
-    if response.status_code == 200:
-        return {"message": "Trade signal received and sent to Telegram."}
+
+@app.get("/quote/{symbol}")
+async def quote(symbol: str = None):
+    if symbol:
+        quote = api.quote(symbol)
     else:
-        raise HTTPException(
-            status_code=500, detail="Failed to send trade signal to Telegram."
-        )
+        quote = api.quote()
+    return quote
+
+
+@app.post("/buy")
+async def buy(order: OrderModel):
+    for i in range(qty_mult):
+        if api.buy(order.symbol, order.volume, order.stoploss, order.takeprofit):
+            tg_body = {
+                "chat_id": TELEGRAM_CHAT_ID,
+                "text": f"📈 BUY {order.symbol} ({order.volume})",
+            }
+            return telegram_request(tg_body)
+        else:
+            raise HTTPException(status_code=500, detail="Buy Order failed.")
+
+
+@app.post("/sell")
+async def sell(order: OrderModel):
+    for i in range(qty_mult):
+        if api.sell(order.symbol, order.volume, order.stoploss, order.takeprofit):
+            tg_body = {
+                "chat_id": TELEGRAM_CHAT_ID,
+                "text": f"📉 SELL {order.symbol} ({order.volume})",
+            }
+            return telegram_request(tg_body)
+        else:
+            raise HTTPException(status_code=500, detail="Sell Order failed.")
+
+
+@app.post("/buyLimit")
+async def buy_limit(order: OrderModel):
+    return {"Order": api.buyLimit(order.symbol, order.volume, order.price)}
+
+
+@app.post("/sellLimit")
+async def sell_limit(order: OrderModel):
+    return {"Order": api.sellLimit(order.symbol, order.volume, order.price)}
+
+
+@app.post("/buyStop")
+async def buy_stop(order: OrderModel):
+    return {"Order": api.buyStop(order.symbol, order.volume, order.price)}
+
+
+@app.post("/sellStop")
+async def sell_stop(order: OrderModel):
+    return {"Order": api.sellStop(order.symbol, order.volume, order.price)}
+
+
+@app.get("/positions")
+async def positions():
+    return api.positions()
+
+
+@app.get("/orders")
+async def orders():
+    return api.orders()
+
+
+@app.post("/orderCancelById")
+async def order_cancel_by_id(id_model: IdModel):
+    api.orderCancelById(id_model.id)
+    return {"message": "Order cancelled"}
+
+
+@app.post("/positionCloseById")
+async def position_close_by_id(id_model: IdModel):
+    api.positionCloseById(id_model.id)
+    return {"message": "Position closed"}
+
+
+@app.post("/cancel_all")
+async def cancel_all():
+    api.cancel_all()
+    return {"message": "All orders cancelled"}
+
+
+@app.post("/close_all")
+async def close_all():
+    api.close_all()
+    return {"message": "All positions closed"}
+
+
+@app.post("/logout")
+async def logout():
+    return {"message": api.logout()}
+
+
+@app.post("/status")
+async def status():
+    return {"connected": api.isconnected()}
+
+
+@app.post("/subscribe")
+async def subscribe(subscription: SymbolModel):
+    for symbol in subscription.symbols:
+        api.subscribe(symbol)
+    return {"message": "Subscribed to symbols"}
